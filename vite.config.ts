ebb37c68fdefafe497b3909f149278371d308cec
Thomas Lebeau <dev.thomaslebeau@gmail.com>
2026-02-21 12:40:37 +0100

    ci: add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
+  base: '/dice_and_card/',
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),

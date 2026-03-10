87a2aa998228d6c310a6c560c2dbf6cde85c0034
Thomas Lebeau <dev.thomaslebeau@gmail.com>
2026-03-10 16:55:22 +0100

    feat: fix path

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/two_dice_left/',
+  base: './',
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
